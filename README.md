65d3e31186016369855d31f2ee3d5d778958cbd1
Michael Medrano <mgmedrano@outlook.com>
2021-06-26 17:45:16 -0700

    README and JS updated

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Probable-Happiness
+# Probable-Happiness (Password Generator)
+
+
+## Background
+- At an employees request, they are want a password be generated at random meeting certain criteria to me a strong password to provide greater security. 
+
+
+## Employee Request:
+- When the generate button is clicked they are presented with a series of prompts for password criteria. 
+- Password Criteria Requesting:
+    - Password minimum of 8 characters with a maximum of 128.
+    - Ability to include Upper Case Characters.
+    - Ability to include Lower Case Characters.
+    - Ability to include numerics.
+    - Ability to include Special Characters/Symbols.
+
+## Password Generated:
+- Upon users desire for criteria, a random password will generate meeting those selected critera. (Validation of criteria )
+
+# Probable-Happiness (Password Generator Preview)
+> Site Preview 
+![Placeholder](assets/) 
+
+
+## Repository & Webpage:
+- URL: https://mgmedrano.github.io/Probable-Happiness/
+- GitHub Repository: https://github.com/mgmedrano/Probable-Happiness
--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var constLower = "abcdefghijklmnopqrstuvwxyz";
-var constUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var symbol = "!@#$%^&*()_+<>?/";
+var charLower = ["a","b","c", "d","e", "f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var charUpper = ["A", "B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var symbol = ["!","@","#","$","%","^","&","*","(",")","_","+","<",">","?","/"];
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-}
+  return password;
+
+  }
 
 function generatePassword () {
-    var passwordText = prompt ("Please enter the number of characters you want for your new password. It must be at least 8 characters but no more than 128 characters");
+    var length = prompt ("Please enter the number of characters you want for your new password. It must be at least 8 characters but no more than 128 characters"); 
     var constLower = confirm ("Do you want lowercase in your password?");
     var constUpper = confirm ("Do you want uppercase in your password?");
+    var symbol = confirm ("Do you want special characters?");
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+document.getElementsByClassName("placeholder").textContent = password;
 
